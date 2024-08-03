f9a7ef62552a0a699875c7907d1cb3cb6334ffda
NSSireesha <sireesha_narakuchi@gmail.com>
2024-08-03 21:06:30 +0530

    initial commit

---
+++ src/test/java/Testclass.java
@@ -0,0 +1,17 @@
+public class Testclass {
+
+    public static void reverse(int num) {
+
+        if (num < 10) {
+            System.out.println(num);
+        } else {
+            System.out.println(num % 10);
+            reverse(num / 10);
+        }
+    }
+
+    public static void main(String args[])
+    {
+        reverse(123);
+    }
+}
