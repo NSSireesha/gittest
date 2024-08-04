f7e724052b358c940c435930fb8ce87317348d50
NSSireesha <sireesha_narakuchi@gmail.com>
2024-08-04 12:54:25 +0530

    added new file

---
+++ src/test/java/Test2class.java
@@ -0,0 +1,25 @@
+public class Test2class {
+
+
+        public static void main(String[] args) {
+
+
+            // StringBuffer str= new StringBuffer("halloween");
+
+            //System.out.println(str.reverse());
+
+
+            String str= "halloween";
+
+            int len= str.length();
+
+            String rev="";
+            for (int i= str.length()-1; i>0; i--)
+            {
+                rev=rev+str.charAt(i);
+
+                System.out.println(rev);
+            }
+        }
+    }
+}
